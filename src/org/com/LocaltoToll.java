daad235754ed1b18c691fc122decfee8bc1c0a44
vicky1328 <vickneshpandian@gmail.com>
2019-05-08 12:16:49 +0530

    first repo

---
+++ src/org/com/LocaltoToll.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class LocaltoToll {
+	public void first() {
+System.out.println("local");
+	}
+	public void second() {
+System.out.println("stsging area");
+}
+	public void third() {
+System.out.println("tool repository");
+	}
+	public static void main(String[] args) {
+		LocaltoToll ll=new LocaltoToll();
+		ll.first();
+		ll.second();
+		ll.third();
+	}
+}
